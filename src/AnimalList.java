f89dea198678ddac9745657277071bd41b05e82b
Drew <drewbdoo@gmail.com>
2022-05-11 14:31:01 -0400

    small changes

--- src/AnimalList.java
+++ src/AnimalList.java
@@ -5,23 +5,25 @@ public class AnimalList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Animal Menu" + "\n"+"Entering nothing will stop the loop");
-        ArrayList<Object> list = new ArrayList<>();
+        ArrayList<Animal> list = new ArrayList<>();
         while(true){
             System.out.println("Please enter a name: ");
             String name = scanner.nextLine();
-            System.out.println("Is it a dog? Yes or no: ");
-            String doggy = scanner.nextLine();
-            boolean dog;
             if(name.equalsIgnoreCase("")){
                 break;
             }
-            dog= doggy.equalsIgnoreCase("yes");
-            list.add(String.valueOf(new Animal(name, dog)));
+            System.out.println("Is it a dog? Yes or no: ");
+            String doggy = scanner.nextLine();
+            boolean dog;
+            dog=doggy.equalsIgnoreCase("yes");
+            list.add(new Animal(name, dog));
 
           }
-        for (Object o : list) {
-            System.out.println(o.toString());
+        for (Animal animal : list) {
+            System.out.println(animal.toString());
+            System.out.println(animal.getName());
         }
+
 
     }
 }
